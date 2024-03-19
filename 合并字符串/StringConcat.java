2af0b21f67207902def576d5bd9230facca427a5
gtchappy <2425172709@qq.com>
2024-03-20 02:09:32 +0800

    第一次代码收藏提交

---
+++ 合并字符串/StringConcat.java
@@ -0,0 +1,39 @@
+package 合并字符串;
+
+import java.util.Scanner;
+
+/**
+ * ClassName: StringConcat
+ * Package: 合并字符串
+ * Description:
+ *
+ * @Author gtc
+ * @Create 2024/3/20 1:53
+ * @Version 1.0
+ */
+public class StringConcat {
+    public static void main(String[] args) {
+        StringConcat test = new StringConcat();
+        Scanner scanner = new Scanner(System.in);
+        //接收链接字符
+        System.out.println("请输入想要用来链接的字符");
+        String operator = scanner.nextLine();
+        //接收拼接字符
+        System.out.println("请输入想要用来拼接的字符,用“,”分隔");
+        String inputString = scanner.nextLine();
+        String[] splitString = inputString.split(",");
+        System.out.println(test.concat(operator, (Object[]) splitString));
+    }
+
+    public String concat(String operator, Object... strs) {
+        String result = "";
+        for (int i = 0; i < strs.length; i++) {
+            if (i == 0) {
+                result += strs[i];
+            } else {
+                result += (operator + strs[i]);
+            }
+        }
+        return result;
+    }
+}
---
+++ 输出一串不重复的数/NumNotRepeat.java
@@ -0,0 +1,33 @@
+package 输出一串不重复的数;
+
+/**
+ * ClassName: Numnotrepeat
+ * Package: 输出一串不重复的数
+ * Description:
+ *
+ * @Author gtc
+ * @Create 2024/3/20 2:01
+ * @Version 1.0
+ */
+public class NumNotRepeat {
+    public static void main(String[] args) {
+        //待生成10个数字
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            //生成范围为1~10的数字
+            arr[i] = (int) (Math.random() * 10 + 1);
+            for (int j = 0; j < arr.length; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (arr[i] == arr[j]) {
+                    i -= 1;
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
